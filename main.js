fa68c3fac81b0c338bf5ae8aa8a0e60f5acbaa8e
danillo cesar <danillocesar_06@hotmail.com>
2020-07-08 17:44:43 -0300

    classes e métodos

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 class List{
     constructor(){ 
         this.data = [];
@@ -10,11 +11,36 @@ class List{
 }
 
 class TodoList extends List{
+    constructor(){
+        //chama o método constructor da classe pai
+        super();
+        this.usuario = 'Danillo';
+    }
 
+    mostraUser(){
+        console.log(this.usuario);
+    }
 }
 
-const MinhaLista = new TodoList();
+var MinhaLista = new TodoList();
 
 document.getElementById('addTodo').onclick = function(){
     MinhaLista.add("teste de herança");
-} 
+    MinhaLista.mostraUser();
+}
+*/
+/*
+class Matematica{
+
+    //cria o método estático addTodo()
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(54, 8));
+*/
+
+
+const a = 1;
+ 
